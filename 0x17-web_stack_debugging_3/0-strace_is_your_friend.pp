230e37b1293ebbb86c53fb8a25061117cf8e20ff
Dafinci01 <odelanadavid@gmail.com>
2024-08-18 22:45:30 +0100

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,13 @@
-package { 'libmylib':
-  ensure => installed,
+# A puppet script to correct a cinfig file
+
+$file_path = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'fix_line':
+
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+
+  path    => ['/bin','/usr/bin']
+
 }
